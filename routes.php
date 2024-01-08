053cee84d858b97380a96ac9251e6e8e5580fa0a
unknown <thiagobraganobre@gmail.com>
2024-01-08 17:27:49 -0300

    Business

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 
 $rotas = [
     'GET /' => new MainController(),
-    'GET /login' => new LoginController(),
+    'POST /login' => new LoginController(),
     'GET /beneficio' => new Rh_BeneficioController(),
     'GET /home' => new Rh_BeneficioController(),
     // Adicione mais rotas conforme necessário
